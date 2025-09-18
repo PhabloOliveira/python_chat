327a7439241b4aae238f013088e451f301759f2c
Matheus Meigre <matheusmeigre@Hotmail.com>
2025-09-17 21:54:03 -0300

    adicionando manager e removendo pasta antiga

--- app/main.py
+++
@@ -1,9 +0,0 @@
-# app/main.py
-
-from fastapi import FastAPI
-from .routes import http, websocket
-
-app = FastAPI(title="Chat com websocket e Historico no MongoDB")
-
-app.include_router(http.router)
-#app.include_router(websocket.router)
--- run.py
+++
@@ -1,5 +0,0 @@
-# run.py
-
-import uvicorn
-if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="192.168.254.233", port=8000, reload=True)
